1b93f4eeddffd15073e11eb9a19e2776ae9ba0dd
Kenny Yarboro <kenny.yarboro@gmail.com>
2015-10-03 16:59:55 -0400

    Completed first three tutorial extensions

---
+++ blog/migrations/0003_auto_20151003_1652.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0002_comment'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='comment',
+            old_name='approved_commment',
+            new_name='approved_comment',
+        ),
+    ]
